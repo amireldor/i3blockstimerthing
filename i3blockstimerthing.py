860374a3d7613783b59c57259e8c5306e64e8b5f
Amir Eldor <amir@eldor.dev>
2020-05-01 01:23:04 +0300

    Add WTFPL

--- i3blockstimerthing.py
+++ i3blockstimerthing.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+
+# Copyright © 2020 Amir Eldor <amir@eldor.dev>
+# This work is free. You can redistribute it and/or modify it under the
+# terms of the Do What The Fuck You Want To Public License, Version 2,
+# as published by Sam Hocevar. See the COPYING file for more details.
+
 from datetime import datetime, timedelta
 from os import getenv
 
